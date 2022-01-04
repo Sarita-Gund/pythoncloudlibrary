23a6f57a722dace2296de1ba81c4db8f8584eee9
Sarita-Gund <sarita.gund23@gmail.com>
2022-01-04 17:28:23 +0530

    first commit

---
+++ venv/Scripts/easy_install-script.py
@@ -0,0 +1,12 @@
+#!"C:\Users\Nikhil\Ineuron batch 2\Machine_Learning\project\deployment in azure\18th_full_stack-main\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==40.8.0','console_scripts','easy_install'
+__requires__ = 'setuptools==40.8.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('setuptools==40.8.0', 'console_scripts', 'easy_install')()
+    )
